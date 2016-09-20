6dffd528ef8b871e90fd2985235ad1736161477e
Silv3r <silv3r@openmailbox.org>
2016-09-20 21:37:09 +0200

    Add the AntibansClassroom lib

---
+++ Libs/AntibansClassroom.lua
@@ -0,0 +1,38 @@
+-- Copyright © 2016 Silv3r <silv3r@openmailbox.org>
+-- This work is free. You can redistribute it and/or modify it under the
+-- terms of the Do What The Fuck You Want To Public License, Version 2,
+-- as published by Sam Hocevar. See the LICENSE file for more details.
+
+antibanQuestions = {
+
+["What type is Flygon?"] = "Dragon/Ground",
+["How many Pokemon can Eevee currently evolve into?"] = "8",
+["Which of these are effective against Dragon?"] = "Dragon",
+["What level does Litleo evolve into Pyroar?"] = "35",
+["Articuno is one of the legendary birds of Kanto."] = "True",
+
+}
+
+function onAntibanPathAction()
+	if getMapName() == "Prof. Antibans Classroom" then
+		log("Quiz detected, talking to the prof.")
+		talkToNpc("Prof. Antiban")
+	end
+end
+
+function onAntibanDialogMessage(message)
+	if getMapName() ~= "Prof. Antibans Classroom" then
+		return
+	end
+	if stringContains(message, "incorrect") then
+		fatal("Could not answer correctly, stopping the bot.")
+	end
+	for key, value in pairs(antibanQuestions) do
+		if stringContains(message, key) then
+			pushDialogAnswer(value)
+		end
+	end
+end
+
+registerHook("onPathAction", onAntibanPathAction)
+registerHook("onDialogMessage", onAntibanDialogMessage)
